217734ccc879d62f9b1658474ffbeb829ef2a047
Mircea-Pavel ANTON <contact@mirceanton.com>
2026-04-07 11:02:45 +0300

    feat(cloud): mark public ip outputs as sensitive

--- modules/cloud/outputs.tf
+++ modules/cloud/outputs.tf
@@ -1,14 +1,17 @@
 output "dns_name" {
   description = "The DDNS hostname assigned by MikroTik Cloud (e.g. <serial>.sn.mynetname.net). Only populated when ddns_enabled is true."
   value       = routeros_ip_cloud.this.dns_name
+  sensitive   = true
 }
 
 output "public_address" {
   description = "The public IPv4 address of the router as detected by MikroTik Cloud."
   value       = routeros_ip_cloud.this.public_address
+  sensitive   = true
 }
 
 output "public_address_ipv6" {
   description = "The public IPv6 address of the router as detected by MikroTik Cloud."
   value       = routeros_ip_cloud.this.public_address_ipv6
+  sensitive   = true
 }
